f4169137499506e771cd74ec1ed2653ea72a5574
Kostis Karantias <kkarantias@gmail.com>
2020-02-18 11:02:34 +0200

    generate some address
    but not burn yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use monero::cryptonote::hash::Hash;
+use monero::{Address, Network};
+use monero::util::key::KeyPair;
+
+extern crate hex;
+
 fn main() {
-    println!("Hello, world!");
+    let tag = hex::decode("0badcafe").expect("invalid tag");
+    let view = Hash::hash_to_scalar(&tag);
+    let spend = Hash::hash_to_scalar(&tag);
+    println!("{}", Address::from_keypair(Network::Mainnet, &KeyPair { view, spend }));
 }
