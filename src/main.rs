96f55c240ed6b51a31c9b5ab8dbe64f67c0ff43d
Kostis Karantias <kkarantias@gmail.com>
2020-02-18 12:27:18 +0200

    implement spend key with unknown dlog

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use monero::cryptonote::hash::Hash;
 use monero::{Address, Network};
-use monero::util::key::KeyPair;
+use monero::util::key::PublicKey;
+use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::edwards::CompressedEdwardsY;
+use sha2::Sha512;
 
 extern crate hex;
 
 fn main() {
     let tag = hex::decode("0badcafe").expect("invalid tag");
-    let view = Hash::hash_to_scalar(&tag);
-    let spend = Hash::hash_to_scalar(&tag);
-    println!("{}", Address::from_keypair(Network::Mainnet, &KeyPair { view, spend }));
+    let view = PublicKey::from_private_key(&Hash::hash_to_scalar(&tag));
+    let spend_ristretto = RistrettoPoint::hash_from_bytes::<Sha512>(&tag).compress();
+    let spend = CompressedEdwardsY::from_slice(spend_ristretto.as_bytes());
+    println!("{}", Address::standard(Network::Mainnet, PublicKey { point: spend }, view));
 }
