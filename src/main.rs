35c4935a297d160b693a4d0b4ccc3291d4ecc629
Kostis Karantias <kkarantias@gmail.com>
2020-02-18 12:32:59 +0200

    make into cli app

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,18 @@ use curve25519_dalek::ristretto::RistrettoPoint;
 use curve25519_dalek::edwards::CompressedEdwardsY;
 use sha2::Sha512;
 
+use structopt::StructOpt;
+
 extern crate hex;
 
+#[derive(Debug, StructOpt)]
+struct Cli {
+    tag: String,
+}
+
 fn main() {
-    let tag = hex::decode("0badcafe").expect("invalid tag");
+    let args = Cli::from_args();
+    let tag = hex::decode(&args.tag).expect("invalid tag");
     let view = PublicKey::from_private_key(&Hash::hash_to_scalar(&tag));
     let spend_ristretto = RistrettoPoint::hash_from_bytes::<Sha512>(&tag).compress();
     let spend = CompressedEdwardsY::from_slice(spend_ristretto.as_bytes());
