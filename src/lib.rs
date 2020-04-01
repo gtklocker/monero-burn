4b7937550a36217b6c3f18b0e270cb957c6bf520
Kostis Karantias <kkarantias@gmail.com>
2020-04-01 15:57:54 +0300

    hash & pray with constant tag for spend pk
    Getting an Edwards point out of a Ristretto point proved to be quite a challenge.
    Breaks uncensorability because of the constant tag!

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,27 @@
 use monero::cryptonote::hash::Hash;
 use monero::{Address, Network};
 use monero::util::key::PublicKey;
-use curve25519_dalek::ristretto::RistrettoPoint;
-use curve25519_dalek::edwards::CompressedEdwardsY;
-use sha2::Sha512;
+use num_bigint::BigUint;
+
+const SPEND_TAG: &str = "Whereof one cannot speak, thereof one must be silent";
+
+fn hash_to_pk(x: &[u8]) -> PublicKey {
+    let hash = Hash::hash(&x);
+    let y = BigUint::from_bytes_le(&hash.as_bytes());
+    (0u8..)
+        .filter_map(|inc| PublicKey::from_slice(&(&y + inc).to_bytes_le()).ok())
+        .nth(0)
+        .unwrap()
+}
 
 pub fn gen_burn_addr(network: Network, tag: &[u8]) -> monero::Address {
     let view = PublicKey::from_private_key(&Hash::hash_to_scalar(&tag));
-    let spend_ristretto = RistrettoPoint::hash_from_bytes::<Sha512>(&tag).compress();
-    let spend = CompressedEdwardsY::from_slice(spend_ristretto.as_bytes());
-    Address::standard(network, PublicKey { point: spend }, view)
+    // Using a constant spend tag means our transactions are not uncensorable.
+    // As they all share the same spend public key.
+    // But this homebrew hash-to-curve method may take a very long time to terminate
+    // for some inputs.
+    // TODO: Avoid this hash_to_pk call by having the result be a constant.
+    Address::standard(network, hash_to_pk(SPEND_TAG.as_bytes()), view)
 }
 
 pub fn burn_verify(tag: &[u8], addr: monero::Address) -> bool {
