c595518b9dd55d9ff6abc9876bf97bb5258367b1
Kostis Karantias <kkarantias@gmail.com>
2020-04-06 19:54:06 +0300

    Fix bug where hash_to_pk would not terminate
    BigUint takes 32-byte slices in with .from_bytes_le() but truncates zeroes at the end
    when using .to_bytes_le(), resulting in invalid y-points. For hashes with leading zeroes
    this meant that their .to_bytes_le() representation would never be a valid y-point and
    incrementing would obviously not help.
    
    We now pad all candidate y-values to 32 bytes.

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,28 @@ use num_bigint::BigUint;
 
 const SPEND_TAG: &str = "Whereof one cannot speak, thereof one must be silent";
 
-fn hash_to_pk(x: &[u8]) -> PublicKey {
+fn pad_u256(num: &[u8]) -> Vec<u8> {
+    assert!(num.len() <= 32, "input needs to be a 256-bit bytearray");
+    let mut v: Vec<u8> = num.to_vec();
+    v.resize(32, 0);
+    v
+}
+
+fn hash_to_pk(x: &[u8]) -> Option<PublicKey> {
     let hash = Hash::hash(&x);
     let y = BigUint::from_bytes_le(&hash.as_bytes());
-    (0u8..)
-        .filter_map(|inc| PublicKey::from_slice(&(&y + inc).to_bytes_le()).ok())
+    (0u8..=u8::max_value())
+        .filter_map(|inc| {
+            let encoding = pad_u256(&(&y + inc).to_bytes_le());
+            PublicKey::from_slice(&encoding).ok()
+        })
         .nth(0)
-        .unwrap()
 }
 
 pub fn gen_burn_addr(network: Network, tag: &[u8]) -> monero::Address {
     let view = PublicKey::from_private_key(&Hash::hash_to_scalar(&tag));
     // TODO: Avoid this hash_to_pk call by having the result be a constant.
-    Address::standard(network, hash_to_pk(SPEND_TAG.as_bytes()), view)
+    Address::standard(network, hash_to_pk(SPEND_TAG.as_bytes()).unwrap(), view)
 }
 
 pub fn burn_verify(tag: &[u8], addr: monero::Address) -> bool {
@@ -32,6 +41,10 @@ extern crate quickcheck;
 mod tests {
     use super::*;
     quickcheck! {
+        fn hash_to_pk_terminates(inp: Vec<u8>) -> bool {
+            hash_to_pk(&inp).is_some()
+        }
+
         fn valid_spend(tag: Vec<u8>) -> bool {
             gen_burn_addr(Network::Mainnet, &tag).public_spend.point.decompress().is_some()
         }
